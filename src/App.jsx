274f3f11bd04eb6d94dae152de05f3187eb99362
F-a-b-4 <aruhiii444@gmail.com>
2025-05-20 00:26:52 +0500

    Your descriptive commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-// src/App.jsx
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import { useAuth } from "./context/AuthContext";
+import Navbar from "./components/Navbar";
 
 function App() {
   const { isLoggedIn } = useAuth();
 
   return (
-    <Routes>
-      <Route path="/login" element={!isLoggedIn ? <Login /> : <Navigate to="/dashboard" />} />
-      <Route path="/register" element={<Register />} />
-      <Route
-        path="/dashboard"
-        element={isLoggedIn ? <Dashboard /> : <Navigate to="/login" />}
-      />
-      <Route path="*" element={<Navigate to="/login" />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={!isLoggedIn ? <Login /> : <Navigate to="/dashboard" />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/dashboard"
+          element={isLoggedIn ? <Dashboard /> : <Navigate to="/login" />}
+        />
+        <Route path="/" element={<Navigate to={isLoggedIn ? "/dashboard" : "/login"} />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </>
   );
 }
 
